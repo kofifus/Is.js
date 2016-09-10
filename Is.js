f9250efa0345b52de81adf049f5dcef96dab5049
Kofifus <mikagenic@gmail.com>
2016-09-11 09:30:34 +1000

    Update Is.js

--- Is.js
+++ Is.js
@@ -20,22 +20,27 @@ let Is={
 		if (Is.assert._level==='silent') return;
 
 		let msg;
-		if (conds.length===0) msg='Assert failed - no arguments';
+		if (conds.length<2) msg='Assert failed - invalid arguments';
 		else if (conds.length===1 && conds[0]==='__throw') Is.assert._level=null;
 		else if (conds.length===1 && conds[0]==='__console') Is.assert._level='console';
 		else if (conds.length===1 && conds[0]==='__silent') Is.assert._level='silent';
+
 		else {
 			let stack=(Is.assert._regExp ?  (new Error()).stack.split('\n')  : []), stackMsg=(stack.length>2 ? (new Error()).stack.split('\n')[2].replace(Is.assert._regExp, 'at $1' ) :  '');
-			if (Is.int(conds[0])) {
-				if (conds.length=1 !== conds[0]) msg='Assert failed - invalid args count '+stackMsg;
-				conds.shift();
+			if (Is.int(conds[0])) { 
+				if (conds[0]!==-1 && conds.length-1 !== conds[0]) msg='Assert failed - invalid arguments count '+stackMsg;
 			} else if (Is.arr(conds[0])) {
-				if (! conds[0].every(argNum => Is.int(argNum))) msg='Assert failed - invalid args array '+stackMsg;
-				else if (conds[0].indexOf(conds.length-1)===-1) msg='Assert failed - invalid args count '+stackMsg; 
-				conds.shift();
+				if (! conds[0].every(argNum => Is.int(argNum))) msg='Assert failed - invalid arguments array '+stackMsg;
+				else if (conds[0].indexOf(conds.length-1)===-1) msg='Assert failed - invalid arguments count '+stackMsg; 
+			} else {
+				msg='Assert failed - invalid arguments parameter '+stackMsg;
 			}
-			if (!msg && ! conds.every(cond => Is.bool(cond))) msg='Assert failed - invalid args '+stackMsg;
-			if (!msg && ! conds.every(cond => { return cond; })) msg='Assert failed '+stackMsg;
+
+			if (!msg) {
+				conds.shift();
+				if (!msg && ! conds.every(cond => Is.bool(cond))) msg='Assert failed - invalid args '+stackMsg;
+				if (!msg && ! conds.every(cond => { return cond; })) msg='Assert failed '+stackMsg;
+			}
 		}
 		if (msg) { if (!Is.assert._level) throw msg; else if (console) console.log(msg); };
 	}
