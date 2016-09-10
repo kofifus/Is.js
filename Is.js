8e13f86678c427f5cf902abf35c4bc60332f614c
Kofifus <mikagenic@gmail.com>
2016-09-10 12:36:56 +1000

    Update Is.js

--- Is.js
+++ Is.js
@@ -1,14 +1,25 @@
-let Is={
-	undef(v) { return typeof v === 'undefined'; },
-	void(v) { return typeof v === 'undefined' || v===null; },
-	number(v) { return typeof v==='number'; },
-	integer(v) { return Number.isInteger(v); },
-	string(v) { return typeof v==='string' },
-	bool(v) { return typeof v==='boolean' },
-	symbol(v) { return typeof v==='symbol' },
-	dict(v) { return !Is.void(v) && typeof v==='object' && !Is.array(v) && !(v instanceof Date); },
-	array(v) { return v instanceof Array; },
-	date(v) { return typeof v==='object' && v instanceof Date; },
-	elem(v) { return v instanceof Element; },
-	event(v) { return v instanceof Event; },
+let Is=function(cond) {
+	if (!Is.undef) Is.undef = function(v) { return typeof v === 'undefined'; };
+	if (!Is.void) Is.void = function(v) { return typeof v === 'undefined' || v===null; };
+	if (!Is.num) Is.num = function(v) { return typeof v==='number'; };
+	if (!Is.int) Is.int = function(v) { return Number.isInteger(v); };
+	if (!Is.str) Is.str = function(v) { return typeof v==='string' };
+	if (!Is.bool) Is.bool = function(v) { return typeof v==='boolean' };
+	if (!Is.arr) Is.arr = function(v) { return v instanceof Array; };
+	if (!Is.symbol) Is.symbol = function(v) { return typeof v==='symbol' };
+	if (!Is.dict) Is.dict = function(v) { return !Is.void(v) && typeof v==='object' && !Is.array(v) && !(v instanceof Date); };
+	if (!Is.date) Is.date = function(v) { return typeof v==='object' && v instanceof Date; };
+	if (!Is.elem) Is.elem = function(v) { return v instanceof Element; };
+	if (!Is.event) Is.event = function(v) { return v instanceof Event; };
+
+	// assert
+	if (!Is._assert) Is._assert=function(cond) {
+		if (cond==='enable') { Is._noAsserts=false; return; } else if (cond==='disable') { Is._noAsserts=true; return; }
+		if (Is._noAsserts || cond) return;
+		if (Is.undef(Is._hasStack)) Is._hasStack=!!(new Error().stack);
+  		let myFName=(Is._hasStack ? new Error().stack.split('\n')[3].replace(/^\s+at\s+(.+?)\s.+/g, '$1' ) : 0)+' ';
+  		throw(myFName + 'assert failed');
+	}
+	return Is._assert(cond);
 }
+Is('enable');
