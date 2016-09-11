bad291cf49d2bc80a8eb50c6418a50a661904a74
Kofifus <mikagenic@gmail.com>
2016-09-11 15:42:41 +1000

    Update Is.js

--- Is.js
+++ Is.js
@@ -13,35 +13,22 @@ let Is={
 	date(v) { return typeof v==='object' && v instanceof Date; },
 	elem(v) { return v instanceof Element; },
 	event(v) { return v instanceof Event; },
+}
 
-	// assert
-	assert(...conds) {
-		if (typeof Is.assert._regExp==='undefined') Is.assert._regExp=((new Error()).stack ? /^(?:\s+at\s)(.+?)$/g : null);
-		if (Is.assert._level==='silent') return;
+// assert
+Is.assert = (...conds) => {
+	if (Is.assert._level==='silent') return;
+	if (typeof Is.assert._regExp==='undefined') Is.assert._regExp=((new Error()).stack ? /^(?:\s+at\s)(.+?)$/g : null);
 
-		let msg;
-		if (conds.length<2) msg='Assert failed - invalid arguments';
-		else if (conds.length===1 && conds[0]==='__throw') Is.assert._level=null;
-		else if (conds.length===1 && conds[0]==='__console') Is.assert._level='console';
-		else if (conds.length===1 && conds[0]==='__silent') Is.assert._level='silent';
-
-		else {
-			let stack=(Is.assert._regExp ?  (new Error()).stack.split('\n')  : []), stackMsg=(stack.length>2 ? (new Error()).stack.split('\n')[2].replace(Is.assert._regExp, 'at $1' ) :  '');
-			if (Is.int(conds[0])) { 
-				if (conds[0]!==-1 && conds[0] !==conds.length-1) msg='Assert failed - invalid arguments count '+stackMsg;
-			} else if (Is.arr(conds[0])) {
-				if (! conds[0].every(argNum => Is.int(argNum))) msg='Assert failed - invalid arguments array '+stackMsg;
-				else if (conds[0].indexOf(conds.length-1)===-1) msg='Assert failed - invalid arguments count '+stackMsg; 
-			} else {
-				msg='Assert failed - invalid arguments parameter '+stackMsg;
-			}
-
-			if (!msg) {
-				conds.shift();
-				if (! conds.every(cond => Is.bool(cond))) msg='Assert failed - invalid args '+stackMsg;
-				if (!msg && ! conds.every(cond => { return cond; })) msg='Assert failed '+stackMsg;
-			}
-		}
-		if (msg) { if (!Is.assert._level) throw msg; else if (console) console.log(msg); };
+	let cond, msg;
+	if (conds.length!==1 || !Is.bool(cond=conds[0])) msg='Assert failed - invalid arguments';
+	else if (cond==='__throw') Is.assert._level=null;
+	else if (cond==='__console') Is.assert._level='console';
+	else if (cond==='__silent') Is.assert._level='silent';
+	else {
+		let stack=(Is.assert._regExp ?  (new Error()).stack.split('\n')  : []), stackMsg=(stack.length>2 ? (new Error()).stack.split('\n')[2].replace(Is.assert._regExp, 'at $1' ) :  '');
+		if (! conds.every(cond => { return cond; })) msg='Assert failed '+stackMsg;
 	}
+
+	if (msg) { if (!Is.assert._level) throw msg; else if (console) console.log(msg); }
 }
