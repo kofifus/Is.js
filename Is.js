2f98918f32b39f5147441c4b44854831f8fda55d
Kofifus <mikagenic@gmail.com>
2016-09-12 11:14:50 +1000

    Update Is.js

--- Is.js
+++ Is.js
@@ -20,29 +20,30 @@ let Is={
 }
 
 // assert
-Is.assert = (cond) => {
-	if (Is.assert._level==='disabe') return;
+let Assert = (cond) => {
+	if (Assert._level==='disabe') return;
 	
 	function getStack() { return (new Error()).stack; }
 
-	if (typeof Is.assert._parse==='undefined') {
+	if (typeof Assert._parse==='undefined') {
 		let stack=getStack();
-		if (stack && stack[0]==='E') Is.assert._parse=(s => s.split('\n')[2].replace(/^(?:\s+at\s)(.+?)$/g, 'at $1')); // chrome
-		else if (stack && stack[0]==='I') Is.assert._parse=(s => s.split('\n')[1].replace(/^(.+?)@(.+?)$/g, 'at $1 ($2)')); // firefox
-		else if (stack && stack[0]==='g') Is.assert._parse=(s => s.split('\n')[0].replace(/^(.+?)@(.+?)$/g, 'at $1 ($2)')); // safari
-		else Is.assert._parse=(stack => ''); // can't parse
+		if (stack && stack[0]==='E') Assert._parse=(s => s.split('\n')[2].replace(/^(?:\s+at\s)(.+?)$/g, 'at $1')); // chrome
+		else if (stack && stack[0]==='I') Assert._parse=(s => s.split('\n')[1].replace(/^(.+?)@(.+?)$/g, 'at $1 ($2)')); // firefox
+		else if (stack && stack[0]==='g') Assert._parse=(s => s.split('\n')[0].replace(/^(.+?)@(.+?)$/g, 'at $1 ($2)')); // safari
+		else Assert._parse=(stack => ''); // can't parse
 	}
 
 	let msg;
-	if (cond==='__throw') Is.assert._level=null;
-	else if (cond==='__console') Is.assert._level='console';
-	else if (cond==='__disable') Is.assert._level='disable';
-	else if (Is.bool(cond)) { if (!cond) msg='Assert failed '+Is.assert._parse((new Error()).stack); }
-	else msg='Invalid Is.assert arguments';
+	if (cond==='__throw') Assert._level=null;
+	else if (cond==='__console') Assert._level='console';
+	else if (cond==='__disable') Assert._level='disable';
+	else if (Is.bool(cond)) { if (!cond) msg='Assert failed '+Assert._parse((new Error()).stack); }
+	else msg='Invalid Assert arguments';
 
-	if (msg) { if (!Is.assert._level) throw msg; else if (console) console.log(msg); }
+	if (msg) { if (!Assert._level) throw msg; else if (console) console.log(msg); }
 };
 
 
 if (global) global.Is=Is;
+if (global) global.Assert=Assert;
 }((1,eval)('this')));
